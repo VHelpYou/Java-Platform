76162fbea3a8453793e127865f18d7fc23421182
Chethan Hs <chethan@Chethans-MBP.attlocal.net>
2022-04-19 20:31:06 -0500

    Adding one more example

---
+++ Chapter02/src/ss/training/java/AnArrayDemo.java
@@ -0,0 +1,22 @@
+package ss.training.java;
+/**
+ * This Program to illustrate how array elements are indexed. 
+ * @author chethan
+ *
+ */
+public class AnArrayDemo {
+	
+	public static void main(String[] args) {
+		  
+		   // create an array
+		   int[] age = {12, 4, 5, 2, 5};
+
+		   // access each array elements
+		   System.out.println("Accessing Elements of Array:");
+		   System.out.println("First Element: " + age[0]);
+		   System.out.println("Second Element: " + age[1]);
+		   System.out.println("Third Element: " + age[2]);
+		   System.out.println("Fourth Element: " + age[3]);
+		   System.out.println("Fifth Element: " + age[4]);
+		 }
+}
